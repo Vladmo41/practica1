90a9109dc3838fec5f48799d2d32eae0c1b25f58
Saez1709 <a.saezs.2021@alumnos.urjc.es>
2024-02-20 19:09:41 +0100

    hola

--- src/main/java/es/codeurjc/hellowordvscode/Aerogenerator.java
+++ src/main/java/es/codeurjc/hellowordvscode/Aerogenerator.java
@@ -1,20 +1,58 @@
 package es.codeurjc.hellowordvscode;
 
 public class Aerogenerator {
-    private String ID;
+    private String iD;
     private double latitude;
     private double longitude;
     private int length;
     private int height;
     private double power;
-    public Aerogenerator(String ID,double latitude,double longitude,int length,int height,double power){
+    public Aerogenerator(String iD,double latitude,double longitude,int length,int height,double power){
 
-        this.ID=ID;
+        this.iD=iD;
         this.latitude=latitude;
         this.longitude=longitude;
         this.length=length;
         this.height=height;
         this.power=power;
+
+        
     }
 
+    public String getID(){
+        return iD;
+    }
+    public void setID(String n){
+        iD=n;
+    }
+    public double getlatitude(){
+        return latitude;
+    }
+    public void setlatitude(double n){
+        latitude=n;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+    public void setLongitude(double n){
+        longitude=n;
+    }
+    public int getLength(){
+        return length;
+    }
+    public void setLength(int n){
+        length=n;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public void setHeight(int n){
+        height=n;
+    }
+    public double getPower(){
+        return power;
+    }
+    public void setPower(double n){
+        power=n;
+    }
 }
--- src/main/java/es/codeurjc/hellowordvscode/GreetingController.java
+++ src/main/java/es/codeurjc/hellowordvscode/GreetingController.java
@@ -11,42 +11,51 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-	private ArrayList<EoloPark> eoloparks = new ArrayList<>();
+    private ArrayList<EoloPark> eoloparks = new ArrayList<>();
 
-public GreetingController(){
-		eoloparks.add(new EoloPark("Parque 1", "Madrid", "10", "100", 1, "e"));
+    public GreetingController(){
+        eoloparks.add(new EoloPark("Parque 1", "Madrid", "10", "100", 1, "e"));
         eoloparks.add(new EoloPark("Parque 2", "Barcelona", "20", "200", 2, "j"));
-		eoloparks.add(new EoloPark("Parque 3", "Madrid", "20", "200", 2, "j"));
-}
-
-	@GetMapping("/greeting")
-	public String greeting(Model model) {
-
-		model.addAttribute("name", "World");
-		model.addAttribute("eoloparks", eoloparks);
-		
-		return "greting_template";
-	}
-	@GetMapping("/search")
-    public String search(@RequestParam String city, Model model) {
-		ArrayList<EoloPark> filteredParks = new ArrayList<>();
-    	for (EoloPark park : eoloparks) {
-        	if (park.getCity().equalsIgnoreCase(city)) {
-            	filteredParks.add(park);
-        	}
-    	}
-    model.addAttribute("eoloparks", filteredParks);
-    return "greting_template";
+        eoloparks.add(new EoloPark("Parque 3", "Madrid", "20", "200", 2, "j"));
     }
-	@GetMapping("/new_eolopark")
+
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("name", "World");
+        model.addAttribute("eoloparks", eoloparks);
+        return "greting_template";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam String city, Model model) {
+        ArrayList<EoloPark> filteredParks = new ArrayList<>();
+        for (EoloPark park : eoloparks) {
+            if (park.getCity().equalsIgnoreCase(city)) {
+                filteredParks.add(park);
+            }
+        }
+        model.addAttribute("eoloparks", filteredParks);
+        return "greting_template";
+    }
+
+    @GetMapping("/new_eolopark")
     public String newEoloParkForm() {
         return "new_eolopark";
     }
 
     @PostMapping("/create_eolopark")
-    public String createEoloPark(@RequestParam String name, @RequestParam String city,@RequestParam String latitude, @RequestParam String longitude,@RequestParam int area, @RequestParam String terrain) {
-        EoloPark newPark = new EoloPark(name, city, latitude, longitude, area, terrain);
-        eoloparks.add(newPark);
-        return "redirect:/greeting";
+public String createEoloPark(@RequestParam String name, @RequestParam String city,@RequestParam String latitude, @RequestParam String longitude,@RequestParam int area, @RequestParam String terrain,  Model model) {
+    
+    boolean nameExists = eoloparks.stream().anyMatch(park -> park.getName().equalsIgnoreCase(name));
+    if (nameExists) {
+        
+        model.addAttribute("error", "El nombre del parque ya existe");
+        return "new_eolopark";
     }
+
+    
+    EoloPark newPark = new EoloPark(name, city, latitude, longitude, area, terrain);
+    eoloparks.add(newPark);
+    return "redirect:/greeting";
+}
 }
